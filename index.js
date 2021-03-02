d785a71789b7ae5d5d7b33182da6c78c745ff9a9
Erik Shelton <erikshelton@Eriks-Air.lan1>
2021-03-02 00:37:48 -0500

    pushing main files after git setup

---
+++ index.js
@@ -0,0 +1,27 @@
+//require dotenv module
+require('dotenv').config()
+
+//require discord.js module
+const Discord = require('discord.js')
+
+//require config file
+const { prefix, token } = require('./config.json')
+
+//create a new Discord client
+const client = new Discord.Client()
+
+//when the client is ready, run this code
+//this event will only trigger one time after logging in
+client.once('ready', () => {
+  console.log('Ready!')
+})
+
+client.on('message', (message) => {
+  if (message.content == '!Ping') {
+    //send back "Pong!" to the channel the message was sent in
+    message.channel.send('Pong!')
+  }
+})
+
+//login to Discord with your app's token
+client.login(token)
